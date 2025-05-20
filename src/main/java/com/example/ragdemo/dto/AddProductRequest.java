549c9a235c7187d259a19f9158bc6baf611ccab2
Da9el00 <danielsmidstrup1@gmail.com>
2025-05-20 16:49:36 +0200

    Small clean up

--- src/main/java/com/example/ragdemo/dto/AddProductRequest.java
+++ src/main/java/com/example/ragdemo/dto/AddProductRequest.java
@@ -1,7 +1,4 @@
 package com.example.ragdemo.dto;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
 public record AddProductRequest(String website) {
 }
--- src/main/java/com/example/ragdemo/models/PageContent.java
+++ src/main/java/com/example/ragdemo/models/PageContent.java
@@ -1,7 +1,5 @@
 package com.example.ragdemo.models;
 
-import com.example.ragdemo.service.WebScraperService;
-
 import java.util.List;
 
 public record PageContent(
